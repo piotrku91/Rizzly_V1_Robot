9bf2ecbddef9d0362e2c7d8b37016d2eafaf1599
Piotr <piotrku91@github.com>
2022-08-08 00:29:16 +0200

    moveForwardStandardStepCreate function

--- RizzlyCore/Moves/Inc/MoveForward.hpp
+++ RizzlyCore/Moves/Inc/MoveForward.hpp
@@ -11,12 +11,21 @@
 #include "MovePattern.hpp"
 #include "defines.hpp"
 
+enum class MoveJointType
+{
+    Level,
+    Rotation
+};
+
 class MoveForward : public MovePattern, MovePatternLegsInput
 {
 public:
     MoveForward(LegsVector& legs);
     void run() override;
     virtual ~MoveForward() {};
+
+private:
+    inline bool moveForwardStandardStepCreate(LegsOrder current_leg, LegsVector& legs_vector, MoveJointType move_joint_type, uint16_t target_angle);
     
 
 };
--- RizzlyCore/Moves/Src/MoveForward.cpp
+++ RizzlyCore/Moves/Src/MoveForward.cpp
@@ -10,113 +10,36 @@
 
 MoveForward::MoveForward(LegsVector& legs)
     : MovePatternLegsInput(legs) {
-
     MoveStep step_fl_setmax_level = [this](LegsVector& Legs) {
-        Legs[LegsOrder::FrontLeft]->setLevelAngleWithAcceleration(TypicalAngles::Pos120, 5);
-        HAL_Delay(20);
-        if (Legs[LegsOrder::FrontLeft]->getLevelMoveStatus() == MoveStatus::Finished) {
-            setNextStep();
-            Legs[LegsOrder::FrontLeft]->resetMoveStatus();
-            HAL_Delay(100);
-            return true;
-        };
-        return false;
+        return moveForwardStandardStepCreate(LegsOrder::FrontLeft, Legs, MoveJointType::Level, TypicalAngles::Pos120);
     };
 
     MoveStep step_fl_setmax_rotation = [this](LegsVector& Legs) {
-        Legs[LegsOrder::FrontLeft]->setRotationAngleWithAcceleration(TypicalAngles::Pos120, 5);
-        HAL_Delay(20);
-
-        if (Legs[LegsOrder::FrontLeft]->getRotationMoveStatus() == MoveStatus::Finished) {
-            setNextStep();
-            Legs[LegsOrder::FrontLeft]->resetMoveStatus();
-            HAL_Delay(100);
-            return true;
-        };
-
-        return false;
+        return moveForwardStandardStepCreate(LegsOrder::FrontLeft, Legs, MoveJointType::Rotation, TypicalAngles::Pos120);
     };
 
     MoveStep step_fl_setmin_level = [this](LegsVector& Legs) {
-        legs_[LegsOrder::FrontLeft]->setLevelAngleWithAcceleration(TypicalAngles::Pos0, 5);
-        HAL_Delay(20);
-
-        if (legs_[LegsOrder::FrontLeft]->getLevelMoveStatus() == MoveStatus::Finished) {
-            setNextStep();
-            legs_[LegsOrder::FrontLeft]->resetMoveStatus();
-            HAL_Delay(100);
-            return true;
-        };
-
-        return false;
+        return moveForwardStandardStepCreate(LegsOrder::FrontLeft, Legs, MoveJointType::Level, TypicalAngles::Pos0);
     };
 
     MoveStep step_fl_setmin_rotation = [this](LegsVector& Legs) {
-        legs_[LegsOrder::FrontLeft]->setRotationAngleWithAcceleration(TypicalAngles::Pos0, 5);
-        HAL_Delay(20);
-
-        if (legs_[LegsOrder::FrontLeft]->getRotationMoveStatus() == MoveStatus::Finished) {
-            setNextStep();
-            legs_[LegsOrder::FrontLeft]->resetMoveStatus();
-            HAL_Delay(100);
-            return true;
-        };
-
-        return false;
+        return moveForwardStandardStepCreate(LegsOrder::FrontLeft, Legs, MoveJointType::Rotation, TypicalAngles::Pos0);
     };
 
     MoveStep step_br_setmax_level = [this](LegsVector& Legs) {
-        Legs[LegsOrder::BackRight]->setLevelAngleWithAcceleration(TypicalAngles::Pos120, 5);
-        HAL_Delay(20);
-        if (Legs[LegsOrder::BackRight]->getLevelMoveStatus() == MoveStatus::Finished) {
-            setNextStep();
-            Legs[LegsOrder::BackRight]->resetMoveStatus();
-            HAL_Delay(100);
-            return true;
-        };
-        return false;
+        return moveForwardStandardStepCreate(LegsOrder::BackRight, Legs, MoveJointType::Level, TypicalAngles::Pos120);
     };
 
     MoveStep step_br_setmax_rotation = [this](LegsVector& Legs) {
-        Legs[LegsOrder::BackRight]->setRotationAngleWithAcceleration(TypicalAngles::Pos120, 5);
-        HAL_Delay(20);
-
-        if (Legs[LegsOrder::BackRight]->getRotationMoveStatus() == MoveStatus::Finished) {
-            setNextStep();
-            Legs[LegsOrder::BackRight]->resetMoveStatus();
-            HAL_Delay(100);
-            return true;
-        };
-
-        return false;
+        return moveForwardStandardStepCreate(LegsOrder::BackRight, Legs, MoveJointType::Rotation, TypicalAngles::Pos120);
     };
 
     MoveStep step_br_setmin_level = [this](LegsVector& Legs) {
-        legs_[LegsOrder::BackRight]->setLevelAngleWithAcceleration(TypicalAngles::Pos0, 5);
-        HAL_Delay(20);
-
-        if (legs_[LegsOrder::BackRight]->getLevelMoveStatus() == MoveStatus::Finished) {
-            setNextStep();
-            legs_[LegsOrder::BackRight]->resetMoveStatus();
-            HAL_Delay(100);
-            return true;
-        };
-
-        return false;
+        return moveForwardStandardStepCreate(LegsOrder::BackRight, Legs, MoveJointType::Level, TypicalAngles::Pos0);
     };
 
     MoveStep step_br_setmin_rotation = [this](LegsVector& Legs) {
-        legs_[LegsOrder::BackRight]->setRotationAngleWithAcceleration(TypicalAngles::Pos0, 5);
-        HAL_Delay(20);
-
-        if (legs_[LegsOrder::BackRight]->getRotationMoveStatus() == MoveStatus::Finished) {
-            setNextStep();
-            legs_[LegsOrder::BackRight]->resetMoveStatus();
-            HAL_Delay(100);
-            return true;
-        };
-
-        return false;
+        return moveForwardStandardStepCreate(LegsOrder::BackRight, Legs, MoveJointType::Rotation, TypicalAngles::Pos0);
     };
 
     steps.push_back(step_fl_setmax_level);
@@ -129,6 +52,23 @@ MoveForward::MoveForward(LegsVector& legs)
     steps.push_back(step_br_setmin_rotation);
 
     setStepZero();
+}
+
+bool MoveForward::moveForwardStandardStepCreate(LegsOrder current_leg, LegsVector& legs_vector, MoveJointType move_joint_type, uint16_t target_angle) {
+    if (move_joint_type == MoveJointType::Level) {
+        legs_vector[current_leg]->setLevelAngleWithAcceleration(target_angle, 5);
+    } else {
+        legs_vector[current_leg]->setRotationAngleWithAcceleration(target_angle, 5);
+    }
+
+    HAL_Delay(20);
+    if (legs_vector[current_leg]->getLevelMoveStatus() == MoveStatus::Finished) {
+        setNextStep();
+        legs_vector[current_leg]->resetMoveStatus();
+        HAL_Delay(100);
+        return true;
+    };
+    return false;
 }
 
 void MoveForward::run() {
